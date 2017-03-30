113f35db46408f68653578878cf3675414b7cc97
Michael Romano <michael.romano@lendup.com>
2017-03-29 21:26:34 -0700

    remove secrets from code

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,8 +1,8 @@
 require 'yelp'
 
 Yelp.client.configure do |config|
-  config.consumer_key = '1AZCgtdmWVD8dYFdlPzzDw' # ENV['CONSUMER_KEY']
-  config.consumer_secret = 'Q0HAEdWpap_zmPZQb1wHR-x4b6M' # ENV['CONSUMER_SECRET']
-  config.token = '4F8E_5ABHcq2rvfe8kkpGvShtlD-I2d7' # ENV['TOKEN']
-  config.token_secret = 'NputXloIgxH6aGGBXIW_Mt_CDgg' # ENV['TOKEN_SECRET']
+  config.consumer_key = ENV['CONSUMER_KEY']
+  config.consumer_secret = ENV['CONSUMER_SECRET']
+  config.token = ENV['TOKEN']
+  config.token_secret = ENV['TOKEN_SECRET']
 end
